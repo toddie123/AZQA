e449b1701e89841b764e657fcdeb0fc375eafc9d
Todd Kuebelbeck, Jr <todd.kuebelbeck@gmail.com>
2021-02-01 12:47:44 -0600

    Created excel explorer

--- src/Aztech Tool Tracker.py
+++ src/Aztech Tool Tracker.py
@@ -32,7 +32,7 @@ class Tool(object):
         wb = load_workbook(filename=spreadsheet_path)   # create workbook object from passedthrough filepath
         ws = wb.active  # create worksheet object that is the active sheet of the excel book
 
-        self.ID_col = "c"    # Col that contains IDs default from sheet as of 02/01/2021 can be set externally
+        self.ID_col = "C"    # Col that contains IDs default from sheet as of 02/01/2021 can be set externally
 
     def set_spreadsheet_path(self, spread_path_intput):
         self.spreadsheet = spread_path_intput
@@ -57,6 +57,27 @@ class Tool(object):
 
     def set_sheetrow(self, input_sheetrow):
         self.sheetrow = input_sheetrow
+
+    def find_row(self):
+
+        for row in self.ws.iter_rows(self.ID_col):
+            for cell in row:
+                if cell.value == self.ID:
+                    self.sheetrow = cell.row
+
+                # TODO ADD TERMINATOR
+
+    def set_tool_info(self):
+
+        if self.wb.cell(row = self.sheetrow, column = self.ID_col).value is self.ID:
+            # Creating the name of the tool. Concatenates description (col E) and model (col G)
+            # TODO create column variables for parameters
+            self.tool_type = self.wb.cell(row = self.sheetrow, column = 'E').value + " " + self.wb.cell(row = self.sheetrow, column = 'G')
+
+            self.use_limit = self.wb.cell(row = self.sheetrow, column = 'P').value
+            self.cal_date = self.wb.cell(row = self.sheetrow, column = 'M').value
+            self.cal_exp = self.wb.cell(row = self.sheetrow, column = 'L').value
+            
 
 
 
